0ed508387a88436bce097acb7b94aaf2cda67093
luizfper <luizfper@gmail.com>
2019-07-01 16:24:53 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -12,12 +12,10 @@ class SongsController < ApplicationController
   end
 
 	def create
-		binding.pry
-
-    @song = Song.new(song_params)
+		@song = Song.create(song_params)
 
     if @song.save
-      redirect_to @song
+      redirect_to song_path(@song)
     else
       render :new
     end
@@ -49,7 +47,8 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist_name, :genre)
-  end
+    params.require(:song).permit(:title, :artist_name, :genre_id, note_contents: [])
+	end
+
 end
 
--- app/models/song.rb
+++ app/models/song.rb
@@ -19,4 +19,16 @@ class Song < ActiveRecord::Base
 		self.artist ? self.artist.name : nil
 	end
 
+	def note_contents=(notes)
+    notes.each do |content|
+      if content.strip != ''
+        self.notes.build(content: content)
+      end
+    end
+  end
+
+  def note_contents
+    self.notes.map(&:content)
+	end
+
 end
